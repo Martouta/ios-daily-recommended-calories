a5cc9c059aef98445fab73e1a3e7e7267302c284
Marta Noya <tim4martanoya@gmail.com>
2023-02-15 22:41:51 +0100

    Initial Commit

---
+++ DailyRecommendedCalories/DailyRecommendedCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRecommendedCaloriesApp.swift
+//  DailyRecommendedCalories
+//
+//  Created by Marta Noya Bonilla on 15/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyRecommendedCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
